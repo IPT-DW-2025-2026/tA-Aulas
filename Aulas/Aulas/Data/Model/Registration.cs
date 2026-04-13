35e4b44919bffed32519d9a85151738bfd051e0a
jcnpereira <casimiro@ipt.pt>
2026-04-13 22:54:08 +0100

    conclusão modelo

--- Aulas/Aulas/Data/Model/Registration.cs
+++ Aulas/Aulas/Data/Model/Registration.cs
@@ -12,17 +12,38 @@ namespace Aulas.Data.Model {
    [PrimaryKey(nameof(StudentFK), nameof(CourseFK))]  // PK composta (EF Core >= 7)
    public class Registration {
 
-      public DateTime RegistrationDate { get; set; }
-
+      /// <summary>
+      /// Data em que o aluno se inscreve na disciplina
+      /// </summary>
+      [Display(Name = "Data Inscrição")]
+      [DataType(DataType.Date)]
+      public DateTime RegistrationDate { get; set; } = DateTime.Now.Date;
 
       // FK para Student
       //   [Key, Column(Order = 1)] ----> válido para EF <=6
+      /// <summary>
+      /// FK para o aluno que se inscreve na disciplina
+      /// </summary>
+      [ForeignKey(nameof(Student))] // esta anotação informa a EF que o atributo 'StudentFK' é uma FK em conjunto com o atributo 'Student'
+      [Display(Name = "Estudante")]
       public int StudentFK { get; set; }
+      /// <summary>
+      /// FK para o aluno que se inscreve na disciplina
+      /// </summary>
       public Student Student { get; set; } = null!;
+
 
       // FK para Course
       //   [Key, Column(Order = 2)] ----> válido para EF <=6
+      /// <summary>
+      /// FK para a disciplina em que o aluno se inscreve
+      /// </summary>
+      [ForeignKey(nameof(Course))] // esta anotação informa a EF que o atributo 'CourseFK' é uma FK em conjunto com o atributo 'Course'
+      [Display(Name = "Disciplina")]
       public int CourseFK { get; set; }
+      /// <summary>
+      /// FK para a disciplina em que o aluno se inscreve
+      /// </summary>
       public Course Course { get; set; } = null!;
 
 
